78439ed879792361afa33ee13ba5c475ca929c65
Neil-4617 <wizcap1@gmail.com>
2025-05-09 05:28:12 +0800

    add model and routes for crud funtionality

--- server/index.js
+++ server/index.js
@@ -2,20 +2,27 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import todoRoutes from './routes/Todo.route.js';
 
 dotenv.config();
 const port = process.env.PORT || 5000;
 
 // initialize express
 const app = express();
+app.use(express.json());
 
-app.get("/", (req, res) =>{
-    res.send("Server is ready");
+// routes
+app.use("/api/todos", todoRoutes);
+
+// database connection.
+mongoose.connect(process.env.MONGO_URI);
+mongoose.connection.once('open', () => {
+    console.log('Now connected to local MongoDB server.')
 });
 
-// cors enable
+// middleware cors enable
 app.use(cors())
-
 
 // listen
 app.listen(port, () =>{
